334f91baa8469a6004b3b30c932aac728f7e99b3
Xiao Liu <liux0047@gmail.com>
2017-07-09 10:51:38 +0800

    added side nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
     <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
       <h1 class="md-title">News Explorer</h1>
     </md-toolbar>
+  
+    <md-sidenav class="md-left" ref="leftSidenav">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+      <app-nav-list></app-nav-list>
+  
+    </md-sidenav>
   
     <div class="main-content">
       <app-source-selector></app-source-selector>
@@ -15,6 +28,7 @@
 <script>
 import SourceSelector from './components/SourceSelector.vue';
 import NewsList from './components/NewsList.vue';
+import NavList from './components/NavList.vue';
 import { eventBus } from './main';
 
 export default {
@@ -26,7 +40,13 @@ export default {
   },
   components: {
     'app-news-list': NewsList,
-    'app-source-selector': SourceSelector
+    'app-source-selector': SourceSelector,
+    'app-nav-list': NavList
+  },
+  methods: {
+    toggleLeftSidenav() {
+      this.$refs.leftSidenav.toggle();
+    }
   },
   created() {
     eventBus.$on('sourcesUpdated', (data) => {
---
+++ src/components/NavList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="phone-viewport">
+        <md-list>
+            <md-list-item>
+                <md-icon>library_books</md-icon>
+                <span>News</span>
+            </md-list-item>
+    
+            <md-list-item>
+                <md-icon>trending_up</md-icon>
+                <span>Trending</span>
+            </md-list-item>
+    
+            <md-list-item>
+                <md-icon>favorite</md-icon>
+                <span>My Interest</span>
+            </md-list-item>
+    
+            <md-divider class="md-inset"></md-divider>
+    
+            <md-list-item>
+                <md-avatar>
+                    <img src="https://placeimg.com/40/40/people/1" alt="People">
+                </md-avatar>
+    
+                <span>Alex Nelson</span>
+    
+                <md-button class="md-icon-button md-list-action">
+                    <md-icon class="md-primary">chat_bubble</md-icon>
+                </md-button>
+            </md-list-item>
+    
+            <md-list-item>
+                <md-avatar>
+                    <img src="https://placeimg.com/40/40/people/6" alt="People">
+                </md-avatar>
+    
+                <span>Mary Johnson</span>
+    
+                <md-button class="md-icon-button md-list-action">
+                    <md-icon>chat_bubble</md-icon>
+                </md-button>
+            </md-list-item>
+        </md-list>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
