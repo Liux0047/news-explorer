58db3aa6e5e57ea37786b99a67790e65e223ce7a
Xiao Liu <liux0047@gmail.com>
2017-07-09 12:34:20 +0800

    some stylings

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <md-layout md-flex-xsmall="100" md-flex-medium="100" md-flex-large="25">
+  <md-layout md-flex-xsmall="100" md-flex-medium="50" md-flex-large="25">
     <md-card md-with-hover>
       <md-card-media>
         <img :src="article.urlToImage" alt="Picture">
@@ -7,26 +7,49 @@
   
       <md-card-header>
         <div class="md-title">{{ article.title }}</div>
-        <div class="md-subhead">{{ article.author }}</div>
+        <div class="md-subhead">
+           <md-icon class="md-icon-sm">access_time</md-icon> {{ formatTime(article.publishedAt) }} | {{ article.author }}
+           </div>
       </md-card-header>
   
       <md-card-content>
         {{ article.description }}
       </md-card-content>
+  
+      <md-card-actions>
+        <md-button class="md-primary" @click="openUrl(article.url)">See Full Aritcle</md-button>
+      </md-card-actions>
     </md-card>
   </md-layout>
 </template>
 
 <script>
 export default {
-  props: ['article']
+  props: ['article'],
+  methods: {
+    formatTime(dateTime) {
+      return Math.floor((new Date() - new Date(dateTime)) / 36e5) + 'h';
+    },
+    openUrl(url) {
+      window.open(url,"_blank");
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+$icon-sm-szie: 14px;
 .md-card {
-  margin: 0 4px 16px;
+  margin: 0 0px 16px;
   display: inline-block;
   vertical-align: top;
+
+  .md-icon-sm {
+    font-size: $icon-sm-szie;
+    width: $icon-sm-szie;
+    min-width: $icon-sm-szie;
+    height: $icon-sm-szie;
+    min-height: $icon-sm-szie;
+  }
 }
 </style>
--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
-        <h2 class="md-headline">{{ sourceNames[source] }}</h2>
-        <md-layout md-gutter>
-            <app-news-card v-for="(article, index) in articles" :key="index" :article="article"></app-news-card>
+        <md-toolbar class="md-transparent">
+            <h2 class="md-title" style="flex: 1">{{ sourceNames[source] }}</h2>
+            <md-button class="md-icon-button" md-expand-trigger>
+                <md-icon>keyboard_arrow_down</md-icon>
+            </md-button>
+        </md-toolbar>
+    
+        <md-spinner md-indeterminate v-if="!isDataReady"></md-spinner>
+        <md-layout md-gutter v-if="isDataReady">
+            <app-news-card v-for="(article, index) in TopMostArticles" :key="index" :article="article"></app-news-card>
         </md-layout>
     </div>
 </template>
@@ -16,12 +23,19 @@ export default {
     data() {
         return {
             articles: [],
-            sourceNames: NEWS_SOURCES
+            sourceNames: NEWS_SOURCES,
+            viewALl: false,
+            isDataReady: false
         }
     },
     props: ['source'],
     components: {
         'app-news-card': NewsCard
+    },
+    computed: {
+        TopMostArticles() {
+            return this.viewALl ? this.articles : this.articles.slice(0, 4);
+        }
     },
     created() {
         axios.get(`/api/` + this.source)
@@ -29,6 +43,7 @@ export default {
                 // JSON responses are automatically parsed.
                 console.log(response);
                 this.articles = response.data.articles;
+                this.isDataReady = true;
             })
             .catch(e => {
                 console.log(e);
@@ -36,3 +51,18 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+.md-toolbar {
+    padding: 0;
+    border-bottom: solid 1px #eaecf0;
+    margin-bottom: 1em;
+
+    .md-title:first-child {
+        margin-left: 0;
+    }
+    .md-button.md-icon-button {
+        margin: 0;
+    }
+}
+</style>
