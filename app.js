ed94e05926a8333334418d7c8036b774a95cc0f5
Xiao Liu <liux0047@gmail.com>
2017-07-16 11:00:28 +0800

    change default port

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+    console.log('Example app listening on port ' + app.get('port'));
 });
