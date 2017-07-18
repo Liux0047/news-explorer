5d000769982f493ff7b0c8d683a135c281a37504
Xiao Liu <liux0047@gmail.com>
2017-07-18 23:42:13 +0800

    heroku hack

--- app.js
+++ app.js
@@ -17,3 +17,9 @@ app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function () {
     console.log('Example app listening on port ' + app.get('port'));
 });
+
+// heroku hack
+var http = require("http");
+setInterval(function () {
+    http.get("http://news-explorer.herokuapp.com");
+}, 3000000); // every 50 minutes
